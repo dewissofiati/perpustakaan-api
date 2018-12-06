9e165c2a30000091484ca23586756a9fa392e47a
Dewi Syarah Sofiati <dewissofiati@gmail.com>
2018-12-06 13:07:45 +0700

    add schema master transaksi

---
+++ src/main/resources/db/migration/V2018_12_06_12_58_28__schema-master-transaksi.sql
@@ -0,0 +1,13 @@
+CREATE TABLE transaksi(
+  id  character varying(64) not null primary key,
+  tanggal_pinjam date not null,
+  anggota_id character vaarying(64) not null,
+  nama_petugas character varying(100) not null,
+  waktu_transaksi timestamp not null default now(),
+  is_kembali boolean not null default false,
+  tanggal_kembali timestamp
+);
+
+alter table transaksi
+add constraint fk_transaksi_anggota foreign key (anggota_id)
+references anggota(id) on update cascade on delete restrict;
