aa4a4c10ac3b351bef161bb485c25ec88e88000e
Dewi Syarah Sofiati <dewissofiati@gmail.com>
2018-12-06 12:50:26 +0700

    add data master penerbit

---
+++ src/main/resources/db/migration/V2018_12_05_17_32_43__data-master-penerbit.sql
@@ -0,0 +1,4 @@
+insert into penerbit(id,nama,alamat) values
+('001', 'Erlangga','Jl. Soekarno-Hatta No.554'),
+('002', 'Gramedia','Jl. Palmerah Barat No.29-37'),
+('003', 'Sinar Grafika', 'Jl. Aren III No.25');
