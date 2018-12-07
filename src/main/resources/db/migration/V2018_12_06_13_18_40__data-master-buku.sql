fbb8037aeac4a142906d8e9e82b4de9f5726ea98
Dewi Syarah Sofiati <dewissofiati@gmail.com>
2018-12-07 16:02:19 +0700

    add report controller & data buku

--- src/main/resources/db/migration/V2018_12_06_13_18_40__data-master-buku.sql
+++ src/main/resources/db/migration/V2018_12_06_13_18_40__data-master-buku.sql
@@ -1,3 +1,11 @@
 INSERT INTO buku(id, judul, isbn, stock, tanggal_publish, penerbit_id, rak_id)
 VALUES
-('001','Algoritma dan Pemrograman','1234567','10','2005-10-15','001','001');
+('001','Algoritma dan Pemrograman','1234567','10','2005-10-15','001','001'),
+('002','Belajar Bahasa C','1543216','5','2004-05-04','002','001'),
+('003','Belajar Bahasa Java','1472839','7','2010-09-17','001','001'),
+('004','Instrumentasi dan Pengukuran','1967856','3','2011-01-12','003','001')
+;
+
+INSERT INTO kategori_buku(buku_id, kategori_id)
+VALUES
+('001','001'),('002','002'),('003','001'),('004','003');
