0bf8701f5c462fd246ee56d465321a1dae9a8587
Dewi Syarah Sofiati <dewissofiati@gmail.com>
2018-12-09 09:22:19 +0700

    redesign jasper and add table

--- src/main/java/com/tabeldata/training/controller/ReportController.java
+++ src/main/java/com/tabeldata/training/controller/ReportController.java
@@ -1,6 +1,8 @@
 package com.tabeldata.training.controller;
+import com.tabeldata.training.entity.Kategori;
 import com.tabeldata.training.entity.Rak;
 import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.util.JRLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +13,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -20,21 +24,24 @@ public class ReportController {
 
     @GetMapping("/example")
     public void exampleReport(HttpServletResponse response) throws JRException, IOException {
-        InputStream jasperStream = this.getClass().getResourceAsStream("/jasper/report_contoh.jasper");
+        InputStream jasperStream = this.getClass().getResourceAsStream("/jasper/contoh.jasper");
         Map<String, Object> params = new HashMap<>();
-        params.put("nama", "Dewi Syarah Sofiati");
+        params.put("nama", "Dimas Maryanto");
         params.put("status", false);
-        params.put("saldo", new BigDecimal(20000000));
+        params.put("saldo", new BigDecimal(2000000));
 
         Rak rakBuku = new Rak("001", "Rak buku pemograman", 5);
         params.put("rakBuku", rakBuku);
 
+        List<Rak> listRak = Arrays.asList(rakBuku, rakBuku, rakBuku);
+
         JasperReport jasperReport = (JasperReport) JRLoader.loadObject(jasperStream);
-        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, new JREmptyDataSource());
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, new JRBeanCollectionDataSource(listRak));
         response.setContentType("application/x-pdf");
         response.setHeader("Content-disposition", "inline; filename=example.pdf");
 
         final OutputStream outStream = response.getOutputStream();
         JasperExportManager.exportReportToPdfStream(jasperPrint, outStream);
+
     }
 }
